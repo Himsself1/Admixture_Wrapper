c3f54f4bd8da9674115f8f3051d44bb6153b47e4
Himsself1 <spapadadonakis@gmail.com>
2025-04-14 01:25:15 +0300

    Implemented script that plots cv-errors.

---
+++ plot_CV_error.R
@@ -0,0 +1,65 @@
+# * Libraries
+
+list_of_packages <- c(
+  "ggplot2", "reshape2",
+  "forcats", "ggthemes",
+  "patchwork", "gridExtra",
+  "grid", "argparse",
+  "Cairo"
+)
+
+for (i in list_of_packages) {
+  if (!require(i, character.only = TRUE)) {
+    install.packages(i)
+  }
+  library( i, character.only = T )
+}
+
+parser <- ArgumentParser()
+
+parser$add_argument("-plot_folder",
+  default = T, action = "store", type = "character",
+  help = "Folder of output plots"
+)
+
+parser$add_argument("-input_file",
+  default = T, action = "store", type = "character",
+  help = "Input of CV errors"
+)
+
+parser$add_argument("-name",
+  default = T, action = "store", type = "character",
+  help = "Name of the run"
+)
+
+
+arguments <- parser$parse_args()
+if (!dir.exists(plot_folder)) {
+  dir.create(plot_folder, recursive = T)
+  print("Created plot folder and parents")
+}
+
+to_plot <- read.csv(file = arguments$input_file, header = FALSE)
+
+colanames(to_plot) <- c("K", "Error")
+
+plot_ks <- ggplot(data = to_plot, aes(as.factor(K), Errors, fill = "red")) +
+  geom_point(color = "black", alpha = 0.8, shape = 21, size = 6) +
+  geom_line() +
+  xlab("K") + 
+  theme( 
+    strip.text = element_text(size = 17),
+    axis.title = element_text(size = 18),
+    axis.text = element_text(size = 14),
+    legend.title = element_text(size = 17),
+    legend.text = element_text(size = 14)
+  )
+
+plot_file_name_pdf <- file.path(
+  parser$plot_folder,
+  paste0(c(parser$name, "_cv_errors.pdf"), collapse = '')
+)
+
+Cairo(file = plot_file_name_pdf, type = "pdf", width = 1024, height = 1024)
+print(plot_ks)
+dev.off()
